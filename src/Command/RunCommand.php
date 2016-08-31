8d3e2871883eb2cdb60c378b321e8aa6f84a36bb
Chris Harvey <chris@chrisnharvey.com>
2016-08-31 20:17:57 +0100

    Moving where commands are stored

---
+++ src/Command/RunCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Nimbusoft\Parrot\Command;
+
+use Nimbusoft\Parrot\Extension\AbstractCommand;
+
+class RunCommand extends AbstractCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('run')
+            ->setDescription('Run Parrot using a parrot.yml file');
+    }
+
+    public function handle()
+    {
+        
+    }
+}
