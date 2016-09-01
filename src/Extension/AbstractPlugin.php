3cf76f212f4e6e4667ff4249caee402d6af12474
Chris Harvey <chris@chrisnharvey.com>
2016-09-01 20:24:46 +0100

    Adding configure method to plugins

--- src/Extension/AbstractPlugin.php
+++ src/Extension/AbstractPlugin.php
@@ -3,8 +3,11 @@
 namespace Nimbusoft\Parrot\Extension;
 
 use Nimbusoft\Parrot\Parrot;
+use Symfony\Component\Config\Definition\Builder\ParentNodeDefinitionInterface;
 
 abstract class AbstractPlugin implements PluginInterface
 {
     use ParrotAwareTrait;
+
+    public function configure(ParentNodeDefinitionInterface $config) {}
 }
--- src/Extension/PluginInterface.php
+++ src/Extension/PluginInterface.php
@@ -3,8 +3,11 @@
 namespace Nimbusoft\Parrot\Extension;
 
 use Nimbusoft\Parrot\Parrot;
+use Symfony\Component\Config\Definition\Builder\ParentNodeDefinitionInterface;
 
 interface PluginInterface extends ParrotAwareInterface
 {
     public function register();
+
+    public function configure(ParentNodeDefinitionInterface $config);
 }
