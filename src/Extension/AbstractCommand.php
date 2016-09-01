353a8ffee709978321cd5a0b3bc24df8ad87d350
Chris Harvey <chris@chrisnharvey.com>
2016-09-01 20:24:17 +0100

    Making AbstractCommand parrot aware

--- src/Extension/AbstractCommand.php
+++ src/Extension/AbstractCommand.php
@@ -7,12 +7,9 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-abstract class AbstractCommand extends Command implements CommandInterface
+abstract class AbstractCommand extends Command implements CommandInterface, ParrotAwareInterface
 {
-    public function setParrot(Parrot $parrot)
-    {
-        $this->parrot = $parrot;
-    }
+    use ParrotAwareTrait;
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
