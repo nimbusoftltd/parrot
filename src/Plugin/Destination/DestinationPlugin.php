22d41856439aebb3d685b0f54ad9ebc616ede33b
Chris Harvey <chris@chrisnharvey.com>
2016-09-06 20:15:15 +0100

    Destination plugin shouldn't extend file plugin

--- src/Plugin/Destination/DestinationPlugin.php
+++ src/Plugin/Destination/DestinationPlugin.php
@@ -5,17 +5,42 @@ namespace Nimbusoft\Parrot\Plugin\Destination;
 use RuntimeException;
 use League\Event\EventInterface;
 use Nimbusoft\Parrot\Plugin\File\FilePlugin;
+use Nimbusoft\Parrot\Extension\AbstractPlugin;
+use Symfony\Component\Config\Definition\Builder\ParentNodeDefinitionInterface;
 
-class DestinationPlugin extends FilePlugin
+class DestinationPlugin extends AbstractPlugin
 {
+    public function getName(): string
+    {
+        return 'destination';
+    }
+
     public function register()
     {
         $this->parrot->listen('run', [$this, 'run'], -2500);
     }
 
+    public function configure(ParentNodeDefinitionInterface $config)
+    {
+        $config
+            ->children()
+                ->enumNode('compression')
+                    ->defaultValue('gzip')
+                    ->values(['gzip'])
+                ->end()
+                ->scalarNode('filename')
+                    ->isRequired()
+                ->end()
+            ->end();
+    }
+
     public function run(EventInterface $event)
     {
         $config = $event->getConfig();
+
+        foreach ($this->parrot->getPlugin('file')->getAdapters() as $adapter) {
+
+        }
 
         if ( ! isset($config['destination'])) {
             throw new RuntimeException("'destination' section not found in parrot.yml file");
